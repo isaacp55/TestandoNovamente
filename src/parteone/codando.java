2498d38b70debe8620fcd0036dc566b839a0632f
Isaac Pereira Da Silva <isaacsilva15663@gmail.com>
2025-03-21 13:15:45 -0300

    criando tabuada com laco de repeticao for

--- src/parteone/codando.java
+++ src/parteone/codando.java
@@ -1,9 +1,22 @@
 package parteone;
 
+import java.util.Scanner;
+
 public class codando {
 	
 	public static void main(String[] args) {
 	System.out.println("save family");
+	Scanner scn = new Scanner (System.in);
+	int produto, resultado;
+	
+	System.out.println("Digite um número para a tabuada: ");
+      produto = scn.nextInt();
+	
+	for(int i = 0; i <= 10; i++ ) {
+		resultado = produto * i;
+		System.out.println(produto + " X " + i + " = " + resultado);
+	}
+	
 	
 	}
 }
